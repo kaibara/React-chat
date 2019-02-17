2832a0ab9ebbd9056e645122e431c1b57222971a
Takumi Kaibara <kaibara.miraidenshi@gmail.com>
2019-02-18 02:41:25 +0900

    feat/SettingFirebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
 import ChatApp from './components/ChatApp'
+import auth from './reducers/auth'
+
+const RootReducer = combineReducers({auth});
+const store = createStore(RootReducer);
 
 ReactDOM.render(
-  <ChatApp />,
+  <Provider store={ store }>
+    <ChatApp />
+  </Provider>,
   document.getElementById('root')
 )
