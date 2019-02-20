d9f95abd1b147c38da916a53d881dd87c5dbc3af
Takumi Kaibara <kaibara.miraidenshi@gmail.com>
2019-02-21 01:38:00 +0900

    feat: Setting UserName

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react'
 
  export default class Auth extends Component {
-
    constructor(props) {
     super(props)
-  }
-
+   }
    componentDidMount() {
     this.props.refLogin()
-  }
-
+   }
    render() {
     return (
       <div>
-        <button onClick={this.props.doLogin}>Login</button>
+        <button onClick={this.props.Login}>Login</button>
       </div>
     )
   }
